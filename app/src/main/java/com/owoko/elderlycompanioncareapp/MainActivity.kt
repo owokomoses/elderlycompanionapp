bcfba8499b358133c58a4020ab9426b867021d3f
owokomoses <owokomose@gmail.com>
2023-06-19 16:08:36 +0300

    added files

--- app/src/main/java/com/owoko/elderlycompanioncareapp/MainActivity.kt
+++ app/src/main/java/com/owoko/elderlycompanioncareapp/MainActivity.kt
@@ -5,21 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    lateinit var buttonsignup:Button
+    lateinit var text_sign: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttonsignup=findViewById(R.id.btn_signup)
+        text_sign = findViewById(R.id.text_signup)
 
-        buttonsignup.setOnClickListener{
-            val intent=Intent(this,signupActivity::class.java)
+        text_sign.setOnClickListener {
+            val intent = Intent(this, signupActivity::class.java)
             startActivity(intent)
-        }
+
 
         }
 
     }
+}
 
--- app/src/main/java/com/owoko/elderlycompanioncareapp/signupActivity.kt
+++ app/src/main/java/com/owoko/elderlycompanioncareapp/signupActivity.kt
@@ -1,11 +1,20 @@
 package com.owoko.elderlycompanioncareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class signupActivity : AppCompatActivity() {
+    lateinit var text_signin: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        text_signin = findViewById(R.id.text_signin)
+
+        text_signin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
