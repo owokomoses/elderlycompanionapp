c3e4a6007340806a1f5a01af7afd5990eaec8103
owokomoses <owokomoses@gmail.com>
2023-07-04 16:44:34 +0300

    added files

--- app/src/main/java/com/owoko/elderlycompanioncareapp/HomeActivity.kt
+++ app/src/main/java/com/owoko/elderlycompanioncareapp/HomeActivity.kt
@@ -7,14 +7,21 @@ import android.widget.ImageView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var labtest: ImageView
+    lateinit var logout:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         labtest = findViewById(R.id.lab_test)
+        logout = findViewById(R.id.logout)
 
         labtest.setOnClickListener {
             val intent = Intent(this, LabtestActivity::class.java)
             startActivity(intent)
         }
+        logout.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/owoko/elderlycompanioncareapp/Model.kt
+++ app/src/main/java/com/owoko/elderlycompanioncareapp/Model.kt
@@ -1,4 +1,3 @@
 package com.owoko.elderlycompanioncareapp
 
-class Model(val tittle:String,val Description:String,val img:Int) {
-}
+class Model(val tittle:String,val Description:String,val img:Int) 
