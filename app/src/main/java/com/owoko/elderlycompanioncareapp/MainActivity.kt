a89ea05228fc9e31f62ab6ebbdaadc6f10aa7644
owokomoses <owokomose@gmail.com>
2023-07-04 16:10:03 +0300

    added files

--- app/src/main/java/com/owoko/elderlycompanioncareapp/MainActivity.kt
+++ app/src/main/java/com/owoko/elderlycompanioncareapp/MainActivity.kt
@@ -4,29 +4,47 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textsign: TextView
-    lateinit var signinbtn:Button
-
+    lateinit var Txt_gotoregister:TextView
+    lateinit var edtloginemail:TextInputEditText
+    lateinit var edtloginpassword:TextInputEditText
+    lateinit var btnlogin:Button
+    lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textsign = findViewById(R.id.text_signup)
-        signinbtn=findViewById(R.id.btn_signin)
+        Txt_gotoregister = findViewById(R.id.textView2)
+        edtloginemail = findViewById(R.id.layout_name)
+        edtloginpassword = findViewById(R.id.layout_password)
+        btnlogin = findViewById(R.id.btn_signin)
+        auth = FirebaseAuth.getInstance()
 
-        textsign.setOnClickListener {
-            val intent = Intent(this, signupActivity::class.java)
-            startActivity(intent)
 
-
-        }
-        signinbtn.setOnClickListener {
-            val intent=Intent (this,HomeActivity::class.java)
+        Txt_gotoregister.setOnClickListener {
+            val intent = Intent(this,signupActivity::class.java)
             startActivity(intent)
         }
 
+        btnlogin.setOnClickListener {
+            login()
+        }
+    }
+    private fun login(){
+        val email=edtloginemail.text.toString()
+        val pass= edtloginpassword.text.toString()
+        auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(this){
+            if (it.isSuccessful){
+                val intent =Intent(this,HomeActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(this,"Successfully Logged in",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,"Login failed",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
-
--- app/src/main/java/com/owoko/elderlycompanioncareapp/signupActivity.kt
+++ app/src/main/java/com/owoko/elderlycompanioncareapp/signupActivity.kt
@@ -1,20 +1,67 @@
 package com.owoko.elderlycompanioncareapp
 
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class signupActivity : AppCompatActivity() {
-    lateinit var text_signin: TextView
+    lateinit var Txt_gotologin:TextView
+    lateinit var edtemail:TextInputEditText
+    lateinit var edtpass:TextInputEditText
+    lateinit var Btnreg:Button
+    lateinit var edtconfpass:TextInputEditText
+     lateinit var auth:FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-        text_signin = findViewById(R.id.text_signin)
 
-        text_signin.setOnClickListener {
+        Txt_gotologin = findViewById(R.id.textView2)
+        edtemail = findViewById(R.id.layout_name2)
+        edtpass = findViewById(R.id.layout_password2)
+        edtconfpass = findViewById(R.id.layout_password3)
+        Btnreg = findViewById(R.id.btn_signin)
+        auth = Firebase.auth
+
+        Txt_gotologin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        Btnreg.setOnClickListener {
+            Signupuser()
+
+        }
     }
+    private fun Signupuser(){
+        val email=edtemail.text.toString()
+        val pass =edtpass.text.toString()
+        val confirmpass=edtconfpass.text.toString()
+
+        if (email.isBlank()||pass.isBlank()||confirmpass.isBlank()){
+            Toast.makeText(this,"please password and email cant be blank", Toast.LENGTH_LONG).show()
+        }else if(pass != confirmpass){
+            Toast.makeText(this,"Password do not match", Toast.LENGTH_LONG).show()
+        }
+        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this){
+            if (it.isSuccessful){
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(this,"Signed up successfully", Toast.LENGTH_LONG).show()
+                finish()
+            }else{
+                Toast.makeText(this,"Failed to create user", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
 }
